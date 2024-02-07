c2efc5e40f40ec604898de6b5426e13200f9c65e
Bryan <seucleito123456@gmail.com>
2024-02-07 11:26:35 -0300

    java script basico

---
+++ aritmeticos.js
@@ -0,0 +1,21 @@
+console.log(" a soma entre 8 e 7 é", 8 + 7);
+//efetuar a soma de dois numeros informados por voce (var nome variavel = 12
+var n1 = 8;
+var n2 = 9;
+
+var soma = n1 + n2;
+var subt = n1 - n2;
+var div = n1 / n2;
+var mult = n1 * n2;
+
+console.log(" a soma de ", n1, "+", n2, "é igual a :", n1 + n2);
+
+console.log("soma =", soma);
+console.log("subtração =", subt);
+console.log("divisão =", div);
+console.log("multiplicação =", mult);
+
+if (soma >= 10);
+{
+  console.log("resultado acima de 10");
+}
---
+++ variaveis.js
@@ -0,0 +1,7 @@
+console.log("ola");
+
+var nome = "Bryan";
+var idade = 16;
+
+console.log("meu nome é ", nome, "e tenho ", idade, "anos");
+console.log("em agosto irei fazer ", idade + 1 ,"anos");
